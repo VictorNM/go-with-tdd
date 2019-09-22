cd81e40905684511d4e71dd096ef2af4979edeb8
victornm <victor.nguyenmau@gmail.com>
2019-09-22 10:31:55 +0000

    extract constant for Hello

--- hello/hello.go
+++ hello/hello.go
@@ -1,5 +1,17 @@
 package hello
 
-func Hello(name string) string  {
-	return "Hello " + name
+const spanish = "Spanish"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+
+func Hello(name, language string) string  {
+	if name == "" {
+		name = "World"
+	}
+
+	if language == spanish {
+		return spanishHelloPrefix + name
+	}
+
+	return englishHelloPrefix + name
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,11 +3,27 @@ package hello
 import "testing"
 
 func TestHello(t *testing.T)  {
-	expected := "Hello Victor"
-	actual := Hello("Victor")
-
-	if expected != actual {
-		// For tests %q is very useful as it wraps your values in double quotes.
-		t.Errorf("Expect 'Hello' return: %q but it return %q\n", expected, actual)
+	assertCorrectMessage := func(t *testing.T, expected, actual string) {
+		// t.Helper() is needed to tell the test suite that this method is a helper.
+		// By doing this when it fails the line number reported will be in
+		// our function call rather than inside our test helper
+		t.Helper()
+		if expected != actual {
+			// For tests %q is very useful as it wraps your values in double quotes.
+			t.Errorf("Expected %q but actual is %q\n", expected, actual)
+		}
 	}
+
+	// Sometimes it is useful to group tests around a "thing" and then have sub-tests describing different scenarios
+	t.Run("saying hello to people", func(t *testing.T) {
+		assertCorrectMessage(t, "Hello, Victor", Hello("Victor", ""))
+	})
+
+	t.Run("say 'Hello World' when an empty string is supplied", func(t *testing.T) {
+		assertCorrectMessage(t, "Hello, World", Hello("", ""))
+	})
+
+	t.Run("say hello in Spanish", func(t *testing.T) {
+		assertCorrectMessage(t, "Hola, Victor", Hello("Victor", "Spanish"))
+	})
 }
