5eb7cd3637c9fd4757b3fd12ccb605c2f135f6d8
victornm <victor.nguyenmau@gmail.com>
2019-09-24 14:11:01 +0000

    Finish arrays & slices~

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -26,3 +26,26 @@ func TestSumAll(t *testing.T) {
 		t.Errorf("Expected '%v' but actual is '%v'", expected, actual)
 	}
 }
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t *testing.T, expected, actual []int) {
+		t.Helper()
+		if !reflect.DeepEqual(expected, actual) {
+			t.Errorf("Expected '%v' but actual is '%v'", expected, actual)
+		}
+	}
+
+	t.Run("make the sums of some slice", func(t *testing.T) {
+		expected := []int{2, 9}
+		actual := SumAllTails([]int{1, 2}, []int{0, 9})
+
+		checkSums(t, expected, actual)
+	})
+
+	t.Run("safely sum empty slice", func(t *testing.T) {
+		expected := []int{0, 9}
+		actual := SumAllTails([]int{}, []int{3, 4, 5})
+
+		checkSums(t, expected, actual)
+	})
+}
